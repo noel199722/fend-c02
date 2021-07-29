69497dfcf38d414e8c46f4e78dd6e10ab06027f7
Noel <noel199722@gmail.com>
2021-07-29 09:25:13 +0800

    added JS-Dog_Years

---
+++ Codecademy_Exercise/JS-Dog_Years/app.js
@@ -0,0 +1,16 @@
+//my current age
+const myAge = 10;
+//early yeaars
+let earlyYears = 2;
+earkyYears *= 10.5;
+let laterYears = myAge -2;
+laterYears * = 4;
+ 
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Noel".toLowerCase();
+
+console.log(`My Name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
